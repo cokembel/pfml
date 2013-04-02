2df6946de094ccec7a95edfc00d4d9acdce90632
Cole Kembel <cokembel@mines.edu>
2013-04-02 01:40:07 -0600

    Added logout link to user/index.html.erb, and placed the 'New User' link in the login page, which is views/sessions/new.html.erb.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Pfml::Application.routes.draw do
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
-    delete 'logout' => :destroy
+    get 'logout' => :destroy
   end
   
   resources :users
