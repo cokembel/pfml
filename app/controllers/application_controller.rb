7eadc375ea2d0e2124df74748172c827b2ec9b1a
Cole Kembel <cokembel@mines.edu>
2013-04-22 15:07:27 -0600

    Added getUser method to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,17 @@ class ApplicationController < ActionController::Base
   	end
 
   	def getUser
-  		return User.find_by_id(session[:user_id])
+  		if User.find_by_id(session[:user_id])
+  			User.find_by_id(session[:user_id])
+  		else
+  			if not User.find_by_name("Anonymous")
+  				@user = User.new(:name => "Anonymous", :password => "password")
+  				@user.save
+  				return @user
+  			else
+  				return User.find_by_name("Anonymous")
+  			end
+  		end
   	end
 
 end
