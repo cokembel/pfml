ef1071e6aa5b9d6e9ae79f06a1e35863514e4ca1
Cole Kembel <cokembel@mines.edu>
2013-04-22 19:16:51 -0600

    Added gitUser method to application_helper.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
   		end
   	end
 
-  	def getUser
+  	def self.getUser
   		if User.find_by_id(session[:user_id])
   			User.find_by_id(session[:user_id])
   		else
