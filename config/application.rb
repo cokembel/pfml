766fa14b904cbc2a2ca7c6804ab6e31f69934995
Cole Kembel <cokembel@mines.edu>
2013-04-29 00:27:45 -0600

    Addes initialize_on_precompile to application.rb.

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-config.assets.initialize_on_precompile = false
+
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
@@ -57,5 +57,7 @@ module Pfml
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
