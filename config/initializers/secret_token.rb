6f2b4fd6b51067332615c22c721b32ab5230b3cb
Cole Kembel <cokembel@mines.edu>
2013-04-28 21:50:06 -0600

    Added rails app boilerplate.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pfml::Application.config.secret_token = 'e8d8b53d46e91340403bd88813ffa8b046f0987d47e49024c4d8ab2a9e8a0bea68e0817f4f1a8fab2497db3d2f6515cb590fefe99b49b92e853c2c7583523e99'
