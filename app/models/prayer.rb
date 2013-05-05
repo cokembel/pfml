792641760106d114b2ba822cdbbefae799ededb9
Cole Kembel <cokembel@mines.edu>
2013-05-05 13:23:09 -0600

    In Prayer and PrayerRequest models, added validation statements to make sure all fields are present.

--- app/models/prayer.rb
+++ app/models/prayer.rb
@@ -1,4 +1,6 @@
 class Prayer < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :prayer_request
+
+	validates :user, :prayer_request, :presence => true
 end
--- app/models/prayer_request.rb
+++ app/models/prayer_request.rb
@@ -1,4 +1,6 @@
 class PrayerRequest < ActiveRecord::Base
 	belongs_to :user
 	has_many :prayers
+
+	validates :content, :user, :presence => true
 end
